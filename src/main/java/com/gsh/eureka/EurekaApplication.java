bfa5abd16fcfa0f3f026bd151497e1a796dc8356
tonyHua <guoshaohua2010@126.com>
2018-12-15 17:40:18 +0800

    初始化eureka

---
+++ src/main/java/com/gsh/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.gsh.eureka;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+        log.info("eureka启动成功");
+    }
+
+}
+
